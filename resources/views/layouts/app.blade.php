bf7cd37ca14d27ee9969e5e8fa997f17acae39e7
rc2pc2 <petriccarcc@gmail.com>
2023-07-19 11:56:31 +0200

    custom scaffolding with partials and a simple app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Laravel App')</title>
+
+    @vite('resources/js/app.js')
+    @yield('custom-stylesheets')
+    @yield('custom-scripts-head')
 </head>
 <body>
+    @include('partials.header')
 
     <main>
         @yield('main-content')
     </main>
 
-    @vite('resources/js/app.js')
+    @include('partials.footer')
+    @yield('custom-scripts-tail')
 </body>
 </html>
